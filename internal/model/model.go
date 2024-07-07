0460d129c4ee59df8b96e3774f69df5b3cda001e
Divyadarshan Mohapatra <divyadarshandvd007@gmail.com>
2024-07-07 16:40:26 +0530

    Deploy Check

--- internal/model/model.go
+++ internal/model/model.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Note struct {
-	ID 				uuid.UUID 	`gorm:"type:uuid"`
 	Title			string			`json:"title"`
+	ID 				uuid.UUID 	`gorm:"type:uuid"`
 	Subtitle 	string			`json:"subtitle"`
 	Text 			string			`json:"text"`
 	gorm.Model
